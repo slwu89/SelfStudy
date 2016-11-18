111554d0db323c4ccd6ee2c984e213bbae392eff
Sean Wu <slwu89@berkeley.edu>
2016-11-17 20:18:56 -0500

    the undiscovered country

---
+++ LearnCpp/LearnCPP/LearnCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnCPP
+//
+//  Created by Sean Wu on 11/17/16.
+//  Copyright © 2016 Sean Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
